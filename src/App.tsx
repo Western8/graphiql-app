c8255511dfedefd7d3ff631ce041ccc58f4b06a8
Western8 <westernchrome@gmail.com>
2023-12-09 14:12:18 +0300

    feat: setup environment

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import { Routes, Route } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/*
+        <Route path="/unctrl" element={<FormUnctrl />} />
+        <Route path="/ctrl" element={<FormCtrl />} />
+        <Route path="*" element={<Home />} />
+  */}
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
